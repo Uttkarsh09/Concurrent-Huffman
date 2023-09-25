e603b58545ec20bf5ef14318c076f75006fc2a0a
Uttkarsh Patel <uttkarshpatel9@gmail.com>
2023-09-26 00:58:35 +0530

    added define flag during compilation

--- linux.sh
+++ linux.sh
@@ -2,5 +2,6 @@ executable_file="./bin/CPU_Huffman"
 
 clear
 rm $executable_file
-g++ -o $executable_file ./src/cpu/*.cpp ./src/main.cpp -I./include/common/ -I./include/cpu/
+# g++ -o $executable_file ./src/cpu/*.cpp ./src/main.cpp -I./include/common/ -I./include/cpu/
+g++ -o $executable_file ./src/cpu/*.cpp ./src/main.cpp -I./include/common/ -I./include/cpu/ -DTEST
 $executable_file
